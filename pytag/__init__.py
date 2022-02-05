5e02ba25de8e1a8792f147f6e3f9bc906debf2bd
Faiz Jazadi <me@lcat.dev>
2022-02-06 00:22:24 +0700

    Register model in `__init__.py` file
    Signed-off-by: Faiz Jazadi <me@lcat.dev>

--- pytag/__init__.py
+++ pytag/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    from .models import db
+
+    db.init_app(app)
+
     from .views import bp as views_bp  # noqa
 
     app.register_blueprint(views_bp)
