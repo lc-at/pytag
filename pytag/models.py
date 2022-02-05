ebf22793065fe612e487267e062296c2a64e397f
Faiz Jazadi <me@lcat.dev>
2022-02-05 20:56:10 +0800

    add `Project` model
    Signed-off-by: Faiz Jazadi <me@lcat.dev>

---
+++ pytag/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLALchemy
+
+db = SQLAlchemy
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
